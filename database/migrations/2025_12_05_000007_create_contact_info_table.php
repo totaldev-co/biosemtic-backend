73484a65a0d4ba39b020fcde0911008389770a65
Gabriel Castillo <gabriel.castillo03@uptc.edu.co>
2025-12-09 16:19:52 -0500

    refactor: remove default value from schedule field in contact_info table

--- database/migrations/2025_12_05_000007_create_contact_info_table.php
+++ database/migrations/2025_12_05_000007_create_contact_info_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
             $table->string('email')->default('ventas@biosimtec.com.co');
             $table->string('phone')->default('+57 318 5277390');
             $table->string('address')->default('Cra. 80c #25f-34');
-            $table->text('schedule')->default("Lunes - Jueves: 7:00am - 5:00pm\nViernes: 7:00am - 4:00pm");
+            $table->text('schedule');
             $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
